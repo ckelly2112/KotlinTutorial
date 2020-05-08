21535d8714b7703cbd886872f2733e30e1b9f054
unknown <ckelly28@myseneca.ca>
2020-05-08 13:55:32 -0400

    Added reset and plus one functionality

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,14 +6,22 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.roll_button)
+        val countButton: Button = findViewById(R.id.count_up_button)
+        val resetButton: Button = findViewById(R.id.reset_button)
+
         rollButton.setOnClickListener { rollDice() }
+        countButton.setOnClickListener { countUp() }
+        resetButton.setOnClickListener { resetText() }
+
     }
 
     private fun rollDice(){
@@ -22,5 +30,24 @@ class MainActivity : AppCompatActivity() {
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        try {
+            var resultInt = resultText.text.toString().toInt()
+            if (resultInt < 6){
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+
+    } catch (e: NumberFormatException){
+            resultText.text = "1"
+        }
+    }
+
+    private fun resetText(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0"
     }
 }
