20cc51dd69e25fd49cbb1211a0a6024adf547038
unknown <ckelly28@myseneca.ca>
2020-05-08 14:49:05 -0400

    Final version and homework complete

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,13 +12,14 @@ import java.lang.NumberFormatException
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
-
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
 //        val countButton: Button = findViewById(R.id.count_up_button)
         val resetButton: Button = findViewById(R.id.reset_button)
@@ -32,9 +33,20 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice(){
 //        Toast.makeText(this, "button clicked",
 //            Toast.LENGTH_SHORT).show()
+
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun resetText(){
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+    }
+
+    private fun getRandomDiceImage() : Int {
         val randomInt = (1..6).random()
 
-        val drawableResource = when (randomInt) {
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -42,10 +54,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        diceImage.setImageResource(drawableResource)
     }
-
 //    private fun countUp(){
 //        val resultText: TextView = findViewById(R.id.result_text)
 //        try {
@@ -60,7 +69,4 @@ class MainActivity : AppCompatActivity() {
 //        }
 //    }
 //
-    private fun resetText(){
-    diceImage.setImageResource(R.drawable.empty_dice)
-    }
 }
